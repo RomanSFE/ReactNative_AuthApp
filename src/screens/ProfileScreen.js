22d56bf4fbe7c3a3252112571242abe0969c9184
Abdulla Al Roman <romannsu142@gmail.com>
2019-12-06 20:19:28 +0600

    profile page complete

--- src/screens/ProfileScreen.js
+++ src/screens/ProfileScreen.js
@@ -1,11 +1,42 @@
 import React, { Component } from 'react'
-import { Text, View } from 'react-native'
+import { Text, View, Image, StyleSheet, ImageBackground } from 'react-native'
+import * as firebase from 'firebase'
 
 class ProfileScreen extends Component {
+    componentDidMount() {
+        let userId = firebase.auth().currentUser.uid
+
+        return firebase
+        .database().ref('/users/'+ userId)
+        .on('value', function(snapshot){
+            console.log(snapshot.val())
+        })
+    }
+
+    constructor(props){
+        super(props) 
+            this.state = {
+                email: '',
+                firstName: '',
+                lastName: '',
+                age: '',
+                gender: ''
+            }
+    }
+
     render() {
+        const {currentUser} = this.state
         return (
-            <View>
-                <Text> Profile Screen </Text>
+            <View style={{justifyContent: 'center', alignItems: 'center' }}>
+                <Image style={{height: '45%', width: '45%', borderRadius: 5}} 
+                source={require('../components/images/bbg1.jpg')}
+                />
+                <View style={{justifyContent: 'center', alignItems: 'center', marginTop: 20 }}>
+                    <Text style={{color: '#000'}}>{currentUser.firstName}</Text>
+                    <Text style={{color: '#000'}}>Your Email</Text>
+                    <Text style={{color: '#000'}}>Your Age</Text>
+                    <Text style={{color: '#000'}}>Your Gender</Text>
+                </View>
             </View>
         )
     }
--- src/screens/SigninScreen.js
+++ src/screens/SigninScreen.js
@@ -1,19 +1,60 @@
 import React, { Component } from 'react'
-import { Text, View } from 'react-native'
+import { Text, View, ActivityIndicator } from 'react-native'
+import Input from '../components/common/Input'
+import Button from '../components/common/Button'
 import * as firebase from 'firebase'
 
 class SigninScreen extends Component {
+    static navigationOptions = {
+        title: " Sign in your account ",
+        headerStyle: {
+            backgroundColor: '#fff',
+        },
+        headerTintColor: '#333',
+        headerTitleStyle: {
+        fontWeight: 'bold',
+        flex: 1,
+        textAlign: 'left'
+        },   
+    }
+
+    constructor(props){
+        super(props)
+        this.state = {
+            email: '',
+            password: '',
+            loading: false
+        }
+    }
+
     signIn = () =>{
-        const {email, password} = this.this.state
+        const {email, password} = this.state
+        this.setState({
+            loading: true
+        })
+        
 
         firebase.auth().signInWithEmailAndPassword(email, password).then(user =>{
+            this.setState({
+                loading: false
+            })
             this.props.navigation.navigate('Profile')
         })
     }
+
+    handleInput = (key, value) => {
+        this.setState({
+            [key]: value
+        })  
+    }
     render() {
+        const{loading} = this.state
         return (
-            <View>
-                <Text> Sign In Screen </Text>
+            <View style={{ flex: 1, marginTop: 10, marginHorizontal: 20}}>
+                <Input onBlur={this.checkEmail} handleInput={text => this.handleInput('email', text)} placeholder='Enter Your Email' />
+                <Input handleInput={text => this.handleInput('password', text)} placeholder='Enter Your Password' secureTextEntry={true} />
+
+                {loading ? <ActivityIndicator size='small'/> : <Button onPressButton={this.signIn} title="Sign In" style={{ width: 300, borderRadius: 5, marginHorizontal: 10, padding: 14, marginTop: 30, backgroundColor: '#F75B4E'}} /> }
             </View>
         )
     }
