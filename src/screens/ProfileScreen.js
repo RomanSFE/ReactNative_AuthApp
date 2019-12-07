a5d75f9e1f46dd5096ba5a26ef0fd7d4409a7105
Abdulla Al Roman <romannsu142@gmail.com>
2019-12-07 17:00:13 +0600

    auth app full complete

--- src/screens/ProfileScreen.js
+++ src/screens/ProfileScreen.js
@@ -1,44 +1,69 @@
-import React, { Component } from 'react'
-import { Text, View, Image, StyleSheet, ImageBackground } from 'react-native'
-import * as firebase from 'firebase'
 
-class ProfileScreen extends Component {
-    componentDidMount() {
-        let userId = firebase.auth().currentUser.uid
-
-        return firebase
-        .database().ref('/users/'+ userId)
-        .on('value', function(snapshot){
-            console.log(snapshot.val())
-        })
+import React, { Component } from 'react';
+ import { Text, View, Image, StyleSheet, ImageBackground } from 'react-native';
+ import * as firebase from 'firebase';
+ 
+ 
+ class ProfileScreen extends Component {
+    static navigationOptions = {
+        title: " My Profile ",
+        headerStyle: {
+            backgroundColor: '#fff',
+        },
+        headerTintColor: '#333',
+        headerTitleStyle: {
+        fontWeight: 'bold',
+        flex: 1,
+        textAlign: 'left'
+        },   
     }
 
-    constructor(props){
-        super(props) 
-            this.state = {
-                email: '',
-                firstName: '',
-                lastName: '',
-                age: '',
-                gender: ''
-            }
-    }
-
-    render() {
-        const {currentUser} = this.state
-        return (
-            <View style={{justifyContent: 'center', alignItems: 'center' }}>
-                <Image style={{height: '45%', width: '45%', borderRadius: 5}} 
-                source={require('../components/images/bbg1.jpg')}
-                />
-                <View style={{justifyContent: 'center', alignItems: 'center', marginTop: 20 }}>
-                    <Text style={{color: '#000'}}>{currentUser.firstName}</Text>
-                    <Text style={{color: '#000'}}>Your Email</Text>
-                    <Text style={{color: '#000'}}>Your Age</Text>
-                    <Text style={{color: '#000'}}>Your Gender</Text>
-                </View>
-            </View>
-        )
-    }
-}
-export default ProfileScreen
+     componentDidMount() {
+         let userId = firebase.auth().currentUser.uid;
+ 
+ 
+         return firebase
+             .database()
+             .ref('/users/' + userId)
+             .on('value', snapshot => {
+                 console.log(snapshot.val());
+                 this.setState({
+                     currentUser: snapshot.val(),
+                 });
+             });
+     }
+ 
+ 
+     constructor(props) {
+         super(props);
+         this.state = {
+             email: '',
+             firstName: '',
+             lastName: '',
+             age: '',
+             gender: '',
+             currentUser: '',
+         };
+     }
+ 
+ 
+     render() {
+         const { currentUser } = this.state;
+         console.log('currentUser ', currentUser);
+         return (
+             <View style={{ justifyContent: 'center', alignItems: 'center' }}>
+                 <Image
+                     style={{ height: '45%', width: '45%', borderRadius: 5 }}
+                     source={require('../components/images/profile.jpg')}
+                 />
+                 <View style={{ justifyContent: 'center', alignItems: 'center', marginTop: 20 }}>
+                     <Text style={{ color: 'blue', fontSize: 20, fontWeight: 'bold'}}>{currentUser.firstName} {currentUser.lastName}</Text>
+                     <Text style={{ color: '#000' }}>{currentUser.email}</Text>
+                     <Text style={{ color: '#000' }}>{currentUser.age}</Text>
+                     <Text style={{ color: '#000' }}>{currentUser.gender}</Text>
+                 </View>
+             </View>
+         );
+     }
+ }
+ export default ProfileScreen;
